f77b791132cba81db179b6f394b50090720750a8
pratikshatiwari <47111738+pratikshatiwari@users.noreply.github.com>
2024-12-12 14:39:13 +0530

    Update routes.js

--- routes.js
+++ routes.js
@@ -4,7 +4,7 @@ module.exports = function(app, passport) {
 
     // show the home page (will also have our login links)
     app.get('/', function(req, res) {
-        res.render('index' );
+        res.render('index');
     });
 
     // PROFILE SECTION =========================
